f115852b6c35c8cb0f26d1c2882595424419d848
zakaria <alameddine.zakaria@gmail.com>
2023-10-23 15:55:25 +0300

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'text-search';
-  txt = ` What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-of Lorem Ipsum.`;
-  searchHighlighter(value: any, elem: any) {
+  sampleText = `What is Lorem Ipsum?
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+          type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+          of Lorem Ipsum.`;
+  searchQuery = '';
+  highlightedResults: any[] = [];
+
+
+
+  clearSearch(elem: any) {
+    this.searchQuery = '';
+    this.highlightedResults = [];
     elem.innerHTML = elem.innerText
-      .replace(new RegExp(value.target.value + '(?!([^<]+)?<)', 'gi'),
-        '<mark>$&</mark>'
-      );
-    return elem
+  }
+
+  highlightText(elem: any) {
+    if (this.searchQuery) {
+      elem.innerHTML = elem.innerText
+        .replace(new RegExp(this.searchQuery + '(?!([^<]+)?<)', 'gi'),
+          '<mark class="mark">$&</mark>'
+        );
+      const query = this.searchQuery.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // Escape special characters
+      const regex = new RegExp(`(${query})`, 'gi');
+      this.highlightedResults = this.sampleText.split(regex).filter(x => x.toLowerCase() == this.searchQuery.toLowerCase());
+      console.log(this.highlightedResults);
+    } else {
+      this.highlightedResults = [];
+      elem.innerHTML = elem.innerText
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
